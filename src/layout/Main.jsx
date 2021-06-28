fe48f36887054fd95a3d95d3d86b52d7234b2aa1
andoriweb <andoriweb@gmail.com>
2021-06-28 13:07:29 +0300

    preloader

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Movies} from '../components/Movies';
+import {Preloader} from '../components/Preloader';
 
 class Main extends React.Component {
   state = {
@@ -19,7 +20,7 @@ class Main extends React.Component {
       {
         movies.length ? (
           <Movies movies = {this.state.movies} />
-        ) : <h3>Загрузка...</h3>
+        ) : <Preloader />
       }
   </main>
   }
